542307826d13e83158fa66493c21fe5c918e48b6
ATP <32787144+scenery@users.noreply.github.com>
2022-04-19 01:08:24 +0800

    update openssl to 1.1.1n

--- install-nginx.sh
+++ install-nginx.sh
@@ -26,8 +26,8 @@ install_nginx() {
     # nginx-cache-purge
     # wget https://github.com/FRiCKLE/ngx_cache_purge/archive/2.3.tar.gz
     # tar -zxvf 2.3.tar.gz
-    wget https://www.openssl.org/source/openssl-1.1.1m.tar.gz
-    tar -xzvf openssl-1.1.1m.tar.gz && rm openssl-1.1.1m.tar.gz
+    wget https://www.openssl.org/source/openssl-1.1.1n.tar.gz
+    tar -xzvf openssl-1.1.1n.tar.gz && rm openssl-1.1.1n.tar.gz
     git clone https://github.com/google/ngx_brotli
     cd ngx_brotli && git submodule update --init
     cd /root/nginx-temp
@@ -36,7 +36,7 @@ install_nginx() {
     cd nginx-1.20.2
     ./configure \
         --prefix=/etc/nginx \
-        --with-openssl=../openssl-1.1.1m \
+        --with-openssl=../openssl-1.1.1n \
         --with-openssl-opt='enable-tls1_3' \
         --with-http_v2_module \
         --with-http_ssl_module \
