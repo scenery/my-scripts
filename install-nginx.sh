d422df5bdbbce692ae59706fec19771002291690
ATP <32787144+scenery@users.noreply.github.com>
2022-12-10 20:08:22 +0800

    add ipv6 support

--- install-nginx.sh
+++ install-nginx.sh
@@ -38,6 +38,7 @@ install_nginx() {
         --prefix=/etc/nginx \
         --with-openssl=../openssl-1.1.1s \
         --with-openssl-opt='enable-tls1_3' \
+        --with-ipv6 \
         --with-http_v2_module \
         --with-http_ssl_module \
         --with-http_gzip_static_module \
