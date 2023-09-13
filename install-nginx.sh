e70494f95c2165843d45699ac92616e0b7b9abe4
ATP <32787144+scenery@users.noreply.github.com>
2023-09-13 16:28:42 +0800

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Written on 2021-09-19 by ATP for personal test
+# Written by ATP for personal test
 # Website: https://atpx.com
 # Github: https://github.com/scenery/my-scripts
 
@@ -14,27 +14,30 @@ yellow(){
 }
 
 install_nginx() {
+    if [ -d "$NGXBUILD" ]; then
+        rm -rf $NGXBUILD
+    fi
     SECONDS=0
     green "================Start Installing Nginx==============="
     sleep 1
     NOW=$(date +"%Y-%m-%d")
-    apt install build-essential libpcre3 libpcre3-dev zlib1g-dev gcc make curl ca-certificates git wget -y
+    apt update
+    apt install -y build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev cmake curl git wget
     mkdir /etc/nginx
     mkdir /etc/nginx/conf.d
-    mkdir /root/nginx-temp
-    cd /root/nginx-temp
-    wget https://www.openssl.org/source/openssl-1.1.1t.tar.gz
-    tar -xzvf openssl-1.1.1t.tar.gz && rm openssl-1.1.1t.tar.gz
-    git clone https://github.com/google/ngx_brotli
-    cd ngx_brotli && git submodule update --init
-    cd /root/nginx-temp
-    wget https://nginx.org/download/nginx-1.24.0.tar.gz
-    tar xf nginx-1.24.0.tar.gz && rm nginx-1.24.0.tar.gz
-    cd nginx-1.24.0
+    mkdir $NGXBUILD && cd $NGXBUILD
+    wget https://nginx.org/download/$NGXVER.tar.gz
+    tar -xzvf $NGXVER.tar.gz && rm $NGXVER.tar.gz
+    git clone --recurse-submodules -j8 https://github.com/google/ngx_brotli
+    cd ngx_brotli/deps/brotli
+    mkdir out && cd out
+    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_CXX_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_INSTALL_PREFIX=./installed ..
+    cmake --build . --config Release --target brotlienc
+    cd $NGXBUILD/$NGXVER
+    export CFLAGS="-m64 -march=native -mtune=native -Ofast -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections"
+    export LDFLAGS="-m64 -Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
     ./configure \
         --prefix=/etc/nginx \
-        --with-openssl=../openssl-1.1.1t \
-        --with-openssl-opt='enable-tls1_3' \
         --with-http_v2_module \
         --with-http_ssl_module \
         --with-http_gzip_static_module \
@@ -70,9 +73,11 @@ EOF
     systemctl daemon-reload
     systemctl enable nginx.service
     systemctl start nginx.service
+    ln -s /etc/nginx/sbin/nginx /usr/local/bin
+    /etc/nginx/sbin/nginx -V
     green "================Nginx Install Success================"
     green "Program Path: /etc/nginx/"
-    green "Temp files: /root/nginx-temp"
+    green "Temp files: $NGXBUILD"
     green "Status: service nginx status"
     yellow "Total: $SECONDS seconds"
     green "====================================================="
@@ -80,8 +85,8 @@ EOF
 
 clean_temp() {
     echo
-    if [ -d "/root/nginx-temp" ]; then
-        rm -rf /root/nginx-temp
+    if [ -d "$NGXBUILD" ]; then
+        rm -rf $NGXBUILD
     fi
     green "Cleanup done!"
     echo
@@ -89,16 +94,19 @@ clean_temp() {
 
 main() {
     if [ $(id -u) != "0" ]; then
-        red "Error: You must be root to run this script."
+        red "Error: This script must be run as root."
         exit 1
     fi   
     if [ ! -f /etc/debian_version ]; then
         red "Error: This script only supports Debian GNU/Linux Operating System."
         exit 1
     fi
+    NGXSTABLE=$(curl -s https://nginx.org/packages/debian/pool/nginx/n/nginx/ | grep '"nginx_' | sed -n "s/^.*\">nginx_\(.*\)\~.*$/\1/p" |sort -Vr |head -1| cut -d'-' -f1)
+    NGXVER="nginx-"$NGXSTABLE
+    NGXBUILD=/home/nginx-build
     # clear
     green "+---------------------------------------------------+"
-    green "| A tool to auto-compile & install Nginx-1.24.0     |"
+    green "| A tool to auto-compile & install $NGXVER     |"
     green "| Author : ATP <https://atpx.com>                   |"
     green "| Github : https://github.com/scenery/my-scripts    |"
     green "+---------------------------------------------------+"
