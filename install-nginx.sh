df6dd2bba26729ffc70a7dd72eaad7f2529ea168
ATP <32787144+scenery@users.noreply.github.com>
2023-11-02 05:44:10 -0500

    add webdav

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Written by ATP for personal test
+# Written by ATP for personal use
 # Website: https://atpx.com
 # Github: https://github.com/scenery/my-scripts
+
+NGXSTABLE=$(curl -s https://nginx.org/packages/debian/pool/nginx/n/nginx/ | grep '"nginx_' | sed -n "s/^.*\">nginx_\(.*\)\~.*$/\1/p" |sort -Vr |head -1| cut -d'-' -f1)
+NGXVER="nginx-"$NGXSTABLE
+NGXBUILD=/home/nginx-build-temp
 
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
@@ -25,9 +29,10 @@ install_nginx() {
     apt install -y build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev cmake curl git wget
     mkdir /etc/nginx
     mkdir /etc/nginx/conf.d
-    mkdir $NGXBUILD && cd $NGXBUILD
+    mkdir -p $NGXBUILD && cd $NGXBUILD
     wget https://nginx.org/download/$NGXVER.tar.gz
     tar -xzvf $NGXVER.tar.gz && rm $NGXVER.tar.gz
+    git clone https://github.com/arut/nginx-dav-ext-module
     git clone --recurse-submodules -j8 https://github.com/google/ngx_brotli
     cd ngx_brotli/deps/brotli
     mkdir out && cd out
@@ -44,10 +49,12 @@ install_nginx() {
         --with-http_stub_status_module \
         --with-http_sub_module \
         --with-http_realip_module \
+        --with-http_dav_module \
         --with-stream \
         --with-stream_ssl_module \
         --with-stream_ssl_preread_module \
         --add-module=../ngx_brotli
+        --add-module=../nginx-dav-ext-module
     make && make install
     
 cat > /lib/systemd/system/nginx.service <<-EOF
@@ -101,9 +108,6 @@ main() {
         red "Error: This script only supports Debian GNU/Linux Operating System."
         exit 1
     fi
-    NGXSTABLE=$(curl -s https://nginx.org/packages/debian/pool/nginx/n/nginx/ | grep '"nginx_' | sed -n "s/^.*\">nginx_\(.*\)\~.*$/\1/p" |sort -Vr |head -1| cut -d'-' -f1)
-    NGXVER="nginx-"$NGXSTABLE
-    NGXBUILD=/home/nginx-build
     # clear
     green "+---------------------------------------------------+"
     green "| A tool to auto-compile & install $NGXVER     |"
